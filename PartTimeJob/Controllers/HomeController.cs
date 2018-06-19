608e46d7bb1ac4b74f86bf957a709c2a3160f8cf
Nandathilaka <thilinachamikashtc@gmail.com>
2018-06-19 16:24:36 +0530

    Add MyLoggingFilterAttributes class

--- PartTimeJob/Controllers/HomeController.cs
+++ PartTimeJob/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace PartTimeJob.Controllers
             return View();
         }
 
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -23,8 +24,37 @@ namespace PartTimeJob.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            ViewBag.TheMessage = "Having trouble.? Send us a Message";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.TheMessage = "Thanks.!, We got Your Message";
+
+            return View();
+        }
+
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPDOTNETMVC5";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            else {
+                //return Content(serial);
+                //return Json(new { name = "Thilina", value = serial }, JsonRequestBehavior.AllowGet);
+                return RedirectToAction("Index");
+            }
         }
     }
 }
---
+++ PartTimeJob/MyLoggingFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartTimeJob
+{
+    public class MyLoggingFilterAttribute : ActionFilterAttribute
+    {
+        
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var request = filterContext.HttpContext.Request;
+            //Logger.logRequest(request.UserHostAddress);
+            base.OnActionExecuted(filterContext);
+        } 
+    }
+} 
